0ddf40d3f0009f493f89ad4e0699bb398ef2cd10
Agilesh-s <agileshsivakumar.s@gmail.com>
2022-10-27 10:31:19 +0530

    Initial Commit

---
+++ src/main/java/com/nestdigital/officeAppBackend/OfficeAppBackendApplication.java
@@ -0,0 +1,13 @@
+package com.nestdigital.officeAppBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OfficeAppBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OfficeAppBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nestdigital/officeAppBackend/OfficeAppBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nestdigital.officeAppBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OfficeAppBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
